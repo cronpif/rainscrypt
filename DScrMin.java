364419531d6571d2a83a2623d8f48cb15cd2258c
cronpif <m3k4n5@protonmail.com>
2016-06-07 15:33:23 +0700

    Source Code Scrypt Miner On Java
    This Tool write using Netbeans . Issue just on calling miner

---
+++ DScrMin.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scrypt.minner;
+
+import java.applet.Applet;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.AccessController;
+import java.util.Observable;
+import java.util.Observer;
+/**
+ *
+ * @author Administrator
+ */
+public class DScrMin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
